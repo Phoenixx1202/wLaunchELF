c80fb7bf351eaa0b72f6341a17b7715260ad1b16
Maurício <mcnet.fenix@gmail.com>
2025-10-22 00:49:51 -0300

    Rename About_uLE to Sobre_uLE in lang.c

--- lang.c
+++ lang.c
@@ -334,7 +334,7 @@ void Load_External_Language(void)
                     else if (!strcmp(tmp + 1, setting->Misc_Debug_Info + strlen(setting->Misc)))
                         sprintf(setting->LK_Path[i], "%s/%s", LNG(MISC), LNG(Debug_Info));
                     else if (!strcmp(tmp + 1, setting->Misc_About_uLE + strlen(setting->Misc)))
-                        sprintf(setting->LK_Path[i], "%s/%s", LNG(MISC), LNG(About_uLE));
+                        sprintf(setting->LK_Path[i], "%s/%s", LNG(MISC), LNG(Sobre_uLE));
                     else if (!strcmp(tmp + 1, setting->Misc_OSDSYS + strlen(setting->Misc)))
                         sprintf(setting->LK_Path[i], "%s/%s", LNG(MISC), LNG(OSDSYS));
                 }  // end if Misc
@@ -358,7 +358,7 @@ void Load_External_Language(void)
     sprintf(setting->Misc_Load_CNF, "%s/%s", LNG(MISC), LNG(Load_CNF));
     sprintf(setting->Misc_ShowFont, "%s/%s", LNG(MISC), LNG(ShowFont));
     sprintf(setting->Misc_Debug_Info, "%s/%s", LNG(MISC), LNG(Debug_Info));
-    sprintf(setting->Misc_About_uLE, "%s/%s", LNG(MISC), LNG(About_uLE));
+    sprintf(setting->Misc_About_uLE, "%s/%s", LNG(MISC), LNG(Sobre_uLE));
     sprintf(setting->Misc_OSDSYS, "%s/%s", LNG(MISC), LNG(OSDSYS));
 }
 // Ends Load_External_Language
